030d9de1145eeecec20d7266cbc95eba2e7c4560
Poya Faraji <poya.faraji@outlook.com>
2026-07-13 17:32:06 +0330

    [ADD] filtering logic

---
+++ scripts/crad-filter.js
@@ -0,0 +1,45 @@
+const cardsElements = document.querySelectorAll(".mushroom-guide .card");
+const seasonSelectInput = document.getElementById("season");
+const edibileSelectInput = document.getElementById("edible");
+
+const notResultMessageElem = document.querySelector(".no-results-message");
+const currentFilter = {
+  season: seasonSelectInput.value.toLowerCase(),
+  edible: edibileSelectInput.value.toLowerCase(),
+};
+
+const filterCards = () => {
+  const seasonAll = currentFilter.season === "all";
+  const edibleAll = currentFilter.edible === "all";
+
+  let hasVisableCards = false;
+
+  cardsElements.forEach((card) => {
+    const season = card.querySelector("[data-season]").dataset.season;
+    const edible = card.querySelector("[data-edible]").dataset.edible;
+
+    const matchSeason = season === currentFilter.season;
+    const matchEdible = edible === currentFilter.edible;
+
+    if ((matchSeason || seasonAll) && (matchEdible || edibleAll)) {
+      card.hidden = false;
+      hasVisableCards = true;
+    } else {
+      card.hidden = true;
+    }
+
+    notResultMessageElem.hidden = hasVisableCards ? true : false;
+  });
+};
+
+const updateFilter = (e) => {
+  const filterType = e.target.name;
+
+  if (filterType === "season" || filterType === "edible") {
+    currentFilter[filterType] = e.target.value;
+    filterCards();
+  }
+};
+
+seasonSelectInput.addEventListener("change", updateFilter);
+edibileSelectInput.addEventListener("change", updateFilter);
